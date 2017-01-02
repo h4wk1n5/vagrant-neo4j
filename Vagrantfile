a931731c936cfd30fd84a349228275292f752f68
h4wk1n5 <vinnyledezma@gmail.com>
2017-01-02 10:09:10 -0500

    update to use ubuntu xenial

--- Vagrantfile
+++ Vagrantfile
@@ -12,17 +12,19 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.hostname = "neo4j-2M06"
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "precise64"
+  config.vm.box = "ubuntu/xenial64"
 
   # The url from where the 'config.vm.box' box will be fetched if it
   # doesn't already exist on the user's system.
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  #config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  config.vm.box_url = "https://cloud-images.ubuntu.com/releases/xenial/release-20161221/ubuntu-16.04-server-cloudimg-amd64-vagrant.box"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
   # config.vm.network :forwarded_port, guest: 80, host: 8080
   config.vm.network :forwarded_port, guest: 7474, host: 7474
+  config.vm.network :forwarded_port, guest: 7687, host: 7687
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
--- setup.sh
+++ setup.sh
@@ -2,24 +2,31 @@
 
 echo
 echo "Installing Java..."
-cd /usr/local
-wget -nv -O jre-7u45-linux-x64.gz http://javadl.sun.com/webapps/download/AutoDL?BundleId=81812
-tar -xf jre-7u45-linux-x64.gz
-rm jre-7u45-linux-x64.gz
-ln -s /usr/local/jre1.7.0_45/bin/java /usr/bin/java
+#cd /usr/local
+#wget -nv -O jre-7u45-linux-x64.gz http://javadl.sun.com/webapps/download/AutoDL?BundleId=81812
+#tar -xf jre-7u45-linux-x64.gz
+#rm jre-7u45-linux-x64.gz
+#ln -s /usr/local/jre1.7.0_45/bin/java /usr/bin/java
+apt-get install default-jre default-jre-headless -y
+
 
 echo
 echo "Installing Neo4j..."
 cd /etc
-wget -nv http://dist.neo4j.org/neo4j-community-2.2.5-unix.tar.gz
-tar -xf neo4j-community-2.2.5-unix.tar.gz
-rm neo4j-community-2.2.5-unix.tar.gz
-ln -s /etc/neo4j-community-2.2.5/bin/neo4j /usr/bin/neo4j
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | apt-key add -
+echo 'deb http://debian.neo4j.org/repo stable/' >/etc/apt/sources.list.d/neo4j.list
+apt-get update
+apt-get install neo4j -y
+#wget -nv http://dist.neo4j.org/neo4j-community-2.2.5-unix.tar.gz
+#tar -xf neo4j-community-2.2.5-unix.tar.gz
+#rm neo4j-community-2.2.5-unix.tar.gz
+#ln -s /etc/neo4j-community-2.2.5/bin/neo4j /usr/bin/neo4j
 
-echo
-echo "Updating Neo4j Config..."
-sed -i 's/#org\.neo4j\.server\.webserver\.address=0\.0\.0\.0/org.neo4j.server.webserver.address=0.0.0.0/' /etc/neo4j-community-2.2.5/conf/neo4j-server.properties
+#echo
+#echo "Updating Neo4j Config..."
+sed -i 's/#dbms\.connectors\.default_listen_address=0\.0\.0\.0/dbms.connectors.default_listen_address=0.0.0.0:7687/' /etc/neo4j/neo4j.conf
 
 echo
 echo "Starting Neo4j..."
-neo4j start
+ulimit -n 40000
+service neo4j start
